f6bacff401d8c2142250255d8f821b6a64e66ca7
Evandro Jr <evandrojr@gmail.com>
2018-07-05 13:28:42 -0300

    cadastro.js

---
+++ app-ajuda-psiquiatrica/cadastro.js
@@ -0,0 +1,37 @@
+var i = 1;
+var g = 2;
+var f = 3;
+var c = 4;
+
+var s = {
+	i: "Psicoterapia individual",
+	g: "Psicoterapia de Grupo",
+	f: "Psicoterapia Familiar",
+	f: "Psicoterapia Conjugal",
+}
+
+var estabelecimentos = [
+	{nome: "Instituto de Saúde da Faculdade Unijorge",
+	 end: "Av. Luís Viana Filho, 6775, Paralela",
+	 tels: ["3206-8015"],
+	 servs:[s.i, s.g]   	
+	}
+]
+
+
+
+
+
+Serviços: Psicoterapia individual a todas as faixas etárias, Psicodiagnóstico, Avaliação
+Psiconeurológica, Orientação Profissional e Grupos terapêuticos.
+Obs: Gratuito
+Faculdade da Cidade do Salvador
+Endereço: Av. Estados Unidos, 37, 3° andar, Comércio
+Tel: 3254-6916/6943
+Serviços: Psicoterapia individual e em grupo para crianças, adolescentes e adultos.
+Obs: Gratuito ou preço negociado
+Núcleo de Atendimento Psicológico (NAP) da Faculdade Castro Alves/UNIRB
+Endereço: Rua Rubem Berta, 128, Pituba
+Tel: 3033-0009
+Serviços: Psicoterapia individual para crianças, adolescentes e adultos
+Obs: Gratuito
